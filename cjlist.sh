f5da85f83e490da7e554e6c2640e7ca96288dd51
Tarek <34511742+bugpie54@users.noreply.github.com>
2021-02-28 17:09:43 -0500

    update

--- cjlist.sh
+++ cjlist.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#if [[$# -eq 0]]
-#then
-#echo "Usage: >./cjlist.sh <list.txt>"
-#cho "Ex: ./cjlist.sh urls.txt"
-#else
-
+if [[ -z "$1" ]]
+then
+echo "Usage:> ./cjlist.sh <list.txt>"
+echo "Ex: ./cjlist.sh urls.txt"
+else
 for url in $(cat $1)
 do
-	python3 cj.py $url
+python3 cj.py "$url"
 done
 echo "[+] Test Complete!"
+fi
